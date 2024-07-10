cf358c956afd3074944364be69237b114ef139a1
“pnimac” <poornima.chauhan.2019@gmail.com>
2024-07-10 17:10:46 +1000

    Added Config server

---
+++ config-server/src/test/java/com/pnimac/config/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnimac.config.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
