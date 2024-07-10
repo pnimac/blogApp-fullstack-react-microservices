fb6c0a3f4b1f5495d3d97209ee576830822b3f70
“pnimac” <poornima.chauhan.2019@gmail.com>
2024-07-10 17:10:13 +1000

    Added Eureka Discovery server

---
+++ discovery-server/src/test/java/com/pnimac/discovery/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnimac.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
